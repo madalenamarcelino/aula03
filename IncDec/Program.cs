a48f8f1824d8580a639660c573cdc8e25d502975
madalenamarcelino <madelynbennet@gmail.com>
2019-03-15 14:35:32 +0000

    comments

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -6,15 +6,17 @@ namespace IncDec
     {
         static void Main(string[] args)
         {
+            // Variables
             int a = 2;
             int b = -5;
             int c;
             int d;
 
-
+            // 2 different Types of incrementation
             c = a++ + b++;
             d = ++a - b++;
 
+            // Results
             Console.WriteLine("O valor de C e = " + c + " e o valor de D e = " + d);
         }
     }
